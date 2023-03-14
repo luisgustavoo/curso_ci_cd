90c5ef66a02102c70d84cb022d3835b5709588b5
luisgustavo <luisgustavodevbr@gmail.com>
2023-03-13 22:31:09 -0300

    Initial commit

---
+++ bin/curso_ci_cd.dart
@@ -0,0 +1,5 @@
+import 'package:curso_ci_cd/curso_ci_cd.dart' as curso_ci_cd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_ci_cd.calculate()}!');
+}
---
+++ test/curso_ci_cd_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_ci_cd/curso_ci_cd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
